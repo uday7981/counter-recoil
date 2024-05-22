da6eb5711808d5034f9cdc6518886f41dd3bc158
udaykiran <40133834+uday7981@users.noreply.github.com>
2024-05-22 16:03:29 +0530

    added inc, dec and buttons comps

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Button, Card, Typography } from '@mui/material'
 
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,12 +19,60 @@ function App() {
         padding: 20,
         width: 500
       }}>
-        < Typography varint='h5'>Welcome to the counter game</Typography>
+        < Typography variant='h5' display={"flex"} justifyContent={"center"}>Welcome to the counter game</Typography>
+
+
         <br />
+        <Buttons />
+        {/* <CounterComponent /> */}
       </Card>
     </div >
+
 
   )
 }
 
+function Buttons({ count, setCount }) {
+  return <div style={
+    {
+      display: "flex",
+      justifyContent: "space-between"
+    }
+  }>
+
+    <Increase count={count} setCount={setCount} />
+    <Decrease count={count} setCount={setCount} />
+
+
+  </div>
+}
+
+
+function Increase(count, setCount) {
+
+  return <div>
+    <Button variant='contained' onClick={() => {
+      setCount(count + 1)
+    }}>Increase Counter</Button>
+
+  </div>
+
+}
+
+
+function Decrease(count, setCount) {
+  return <div>
+    <Button variant='contained' onClick={() => {
+      setCount(count - 1)
+    }}>Decrease Counter</Button>
+  </div>
+
+
+}
+
+
+function CounterComponent() {
+
+
+}
 export default App
